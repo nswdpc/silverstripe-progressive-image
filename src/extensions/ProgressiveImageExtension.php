b6ce7e1a19e5c18b9a6e80c207b4e868a4a7a2d2
James Ellis <james@host.dpcdev>
2019-03-19 00:14:43 +0000

    Formatting (only)

--- src/extensions/ProgressiveImageExtension.php
+++ src/extensions/ProgressiveImageExtension.php
@@ -1,5 +1,6 @@
 <?php
 namespace NSWDPC\ProgressiveImage;
+
 use Silverstripe\Assets\Image;
 use Silverstripe\Core\Config\Config;
 use Silverstripe\Core\Extension;
@@ -10,160 +11,169 @@ use SilverStripe\ORM\FieldType\DBField;
  * Extension provides methods to aid in progressive image loading techniques, e.g by being able to define quality at request time
  * @author James Ellis <james.ellis@dpc.nsw.gov.au>
  */
-class ProgressiveImageExtension extends Extension {
+class ProgressiveImageExtension extends Extension
+{
+    private $default_quality = 80;// store default for reset post image processing
 
-	private $default_quality = 80;// store default for reset post image processing
+    private $_cache_filter_style = null;
 
-	private $_cache_filter_style = null;
+    /**
+     * Returns the current image backend
+     */
+    private function getBackend()
+    {
+        $backend = $this->owner->getImageBackend();
+        return $backend;
+    }
 
-	/**
-	 * Returns the current image backend
-	 */
-	private function getBackend() {
-		$backend = $this->owner->getImageBackend();
-		return $backend;
-	}
+    /**
+     * Gets a default quality on the backend
+     */
+    private function getDefaultQuality()
+    {
+        $backend = $this->getBackend();
+        return $backend->getQuality();
+    }
 
-	/**
-	 * Gets a default quality on the backend
-	 */
-	private function getDefaultQuality() {
-		$backend = $this->getBackend();
-		return $backend->getQuality();
-	}
+    /**
+     * Sets a default quality on the backend
+     */
+    private function setDefaultQuality()
+    {
+        $backend = $this->getBackend();
+        $backend->setQuality($this->default_quality);
+    }
 
-	/**
-	 * Sets a default quality on the backend
-	 */
-	private function setDefaultQuality() {
-		$backend = $this->getBackend();
-		$backend->setQuality( $this->default_quality );
-	}
+    /**
+     * Set backend quality
+     * @param int $quality quality 0-100
+     */
+    private function setQuality($quality)
+    {
+        if (!$quality || $quality <= 0 || $quality >= 100) {
+            $quality = $this->getDefaultQuality();
+        }
 
-	/**
-	 * Set backend quality
-	 * @param int $quality quality 0-100
-	 */
-	private function setQuality($quality) {
-		if(!$quality || $quality <= 0 || $quality >= 100 ) {
-			$quality = $this->getDefaultQuality();
-		}
+        $this->default_quality = $this->getDefaultQuality();// store the default quality set in config
+        // set the new quality
+        $backend = $this->getBackend();
+        $backend->setQuality((int) $quality);
+        return $quality;
+    }
 
-		$this->default_quality = $this->getDefaultQuality();// store the default quality set in config
-		// set the new quality
-		$backend = $this->getBackend();
-		$backend->setQuality( (int) $quality );
-		return $quality;
-	}
+    /**
+     * Reset quality in config to previously stored value
+     */
+    private function resetQuality()
+    {
+        $this->setDefaultQuality();
+    }
 
-	/**
-	 * Reset quality in config to previously stored value
-	 */
-	private function resetQuality() {
-		$this->setDefaultQuality();
-	}
+    /**
+     * Get a filter style applied to the tiny URL image
+     */
+    private function getFilterStyle()
+    {
+        if (!is_null($this->_cache_filter_style)) {
+            return $this->_cache_filter_style;
+        }
+        $this->_cache_filter_style = $this->owner->config()->get('pil_filter_style');
+        return $this->_cache_filter_style;
+    }
 
-	/**
-	 * Get a filter style applied to the tiny URL image
-	 */
-	private function getFilterStyle() {
-		if(!is_null($this->_cache_filter_style)) {
-			return $this->_cache_filter_style;
-		}
-		$this->_cache_filter_style = $this->owner->config()->get('pil_filter_style');
-		return $this->_cache_filter_style;
-	}
+    /**
+     * Returns the tag used to load the image, with container and padding block
+     * @param ViewableData $image
+     * @param int $width
+     * @param int $height
+     * @param ViewableData $tiny a version of $image that is tiny
+     */
+    private function AsTag(ViewableData $image, $width, $height, ViewableData $tiny)
+    {
+        $final_url = $image->getURL();
+        if ($height >  0 && $width > 0) {
+            $padding_value = round(($height / $width) * 100, 2);
+        } else {
+            $padding_value = 66.6;
+        }
+        $alt = $this->owner->getTitle();
+        $tiny_url = $tiny->getURL();
+        $data = [
+            'PaddingValue' => $padding_value,
+            'AlternateText' => $alt,
+            'FinalURL' => $final_url,
+            'TinyURL' => $tiny_url,
+            'Style' => $this->getFilterStyle()
+        ];
+        $tag = $this->owner->renderWith('ProgressiveImage', $data);
+        $field = DBField::create_field('HTMLText', $tag);
+        return $field;
+    }
 
-	/**
-	 * Returns the tag used to load the image, with container and padding block
-	 * @param ViewableData $image
-	 * @param int $width
-	 * @param int $height
-	 * @param ViewableData $tiny a version of $image that is tiny
-	 */
-	private function AsTag(ViewableData $image, $width, $height, ViewableData $tiny) {
-		$final_url = $image->getURL();
-		if($height >  0 && $width > 0) {
-			$padding_value = round(($height / $width) * 100, 2);
-		} else {
-			$padding_value = 66.6;
-		}
-		$alt = $this->owner->getTitle();
-		$tiny_url = $tiny->getURL();
-		$data = [
-			'PaddingValue' => $padding_value,
-			'AlternateText' => $alt,
-			'FinalURL' => $final_url,
-			'TinyURL' => $tiny_url,
-			'Style' => $this->getFilterStyle()
-		];
-		$tag = $this->owner->renderWith('ProgressiveImage', $data);
-		$field = DBField::create_field( 'HTMLText', $tag );
-		return $field;
-	}
+    /**
+     * ScaleWidth handler for progressive image loading
+     * @param int $width
+     * @param int $quality quality of larger image
+     * @param boolean $as_tag
+     */
+    public function ProgressiveScaleWidth($width, $quality = 80, $as_tag = true)
+    {
+        $quality = $this->setQuality($quality);
+        $image = $this->owner->ScaleWidth($width);
+        $backend = $this->getBackend();
+        if (method_exists($backend, 'ResetFilters')) {
+            $backend->ResetFilters();
+        }
+        $this->resetQuality($quality);
+        if ($as_tag) {
+            // Rendering as a tag.. create the tiny version
+            $tiny_width = round($width / 10);
+            $tiny_quality = 1;
+            $tiny = $this->ProgressiveScaleWidth($tiny_width, $tiny_quality, false);
 
-	/**
-	 * ScaleWidth handler for progressive image loading
-	 * @param int $width
-	 * @param int $quality quality of larger image
-	 * @param boolean $as_tag
-	 */
-	public function ProgressiveScaleWidth($width, $quality = 80, $as_tag = true) {
-		$quality = $this->setQuality($quality);
-		$image = $this->owner->ScaleWidth($width);
-		$backend = $this->getBackend();
-		if(method_exists($backend, 'ResetFilters')) {
-			$backend->ResetFilters();
-		}
-		$this->resetQuality($quality);
-		if($as_tag) {
-			// Rendering as a tag.. create the tiny version
-			$tiny_width = round($width / 10);
-			$tiny_quality = 1;
-			$tiny = $this->ProgressiveScaleWidth($tiny_width, $tiny_quality, false);
+            $tag = $this->AsTag($image, $width, 0, $tiny);
+            return $tag;
+        } else {
+            return $image;
+        }
+    }
 
-			$tag = $this->AsTag($image, $width, 0, $tiny);
-			return $tag;
-		} else {
-			return $image;
-		}
-	}
+    /**
+     * Fill to width and height with quality
+     * @param int $width
+     * @param int $height
+     * @param int $quality quality of larger image
+     * @param boolean $as_tag
+     */
+    public function ProgressiveFill($width, $height, $quality = 80, $as_tag = true)
+    {
+        $quality = $this->setQuality($quality);
+        $image = $this->owner->Fill($width, $height);
+        $backend = $this->getBackend();
+        if (method_exists($backend, 'ResetFilters')) {
+            $backend->ResetFilters();
+        }
+        $this->resetQuality($quality);
+        if ($as_tag) {
+            // Rendering as a tag.. create the tiny version
+            $tiny_height = round($height / 10);
+            $tiny_width = round($width / 10);
+            $tiny_quality = 1;
+            $tiny = $this->ProgressiveFill($tiny_width, $tiny_height, $tiny_quality, false);
 
-	/**
-	 * Fill to width and height with quality
-	 * @param int $width
-	 * @param int $height
-	 * @param int $quality quality of larger image
-	 * @param boolean $as_tag
-	 */
-	public function ProgressiveFill($width, $height, $quality = 80, $as_tag = true) {
-		$quality = $this->setQuality($quality);
-		$image = $this->owner->Fill($width, $height);
-		$backend = $this->getBackend();
-		if(method_exists($backend, 'ResetFilters')) {
-			$backend->ResetFilters();
-		}
-		$this->resetQuality($quality);
-		if($as_tag) {
-			// Rendering as a tag.. create the tiny version
-			$tiny_height = round($height / 10);
-			$tiny_width = round($width / 10);
-			$tiny_quality = 1;
-			$tiny = $this->ProgressiveFill($tiny_width, $tiny_height, $tiny_quality, false);
+            $tag = $this->AsTag($image, $width, $height, $tiny);
+            return $tag;
+        } else {
+            return $image;
+        }
+    }
 
-			$tag = $this->AsTag($image, $width, $height, $tiny);
-			return $tag;
-		} else {
-			return $image;
-		}
-	}
-
-	/**
-	 * CroppedImage handler -> Fill
-	 * @deprecated
-	 */
-	public function ProgressiveCroppedImage($width, $height, $quality = 75, $as_tag = true) {
-		return $this->ProgressiveFill($width, $height, $quality, $as_tag);
-	}
-
+    /**
+     * CroppedImage handler -> Fill
+     * @deprecated
+     */
+    public function ProgressiveCroppedImage($width, $height, $quality = 75, $as_tag = true)
+    {
+        return $this->ProgressiveFill($width, $height, $quality, $as_tag);
+    }
 }
--- templates/Includes/ProgressiveImageLoaderStyle.ss
+++ templates/Includes/ProgressiveImageLoaderStyle.ss
@@ -1,19 +1,18 @@
 <style type="text/css">
-	.pil-ph {
-		position: relative;
-		overflow: hidden;
-	}
+    .pil-ph {
+        position: relative;
+        overflow: hidden;
+    }
 
-	.pil-ph img {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		transition: opacity 0.2s linear;
-	}
+    .pil-ph img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        transition: opacity 0.2s linear;
+    }
 
-	.pil-ph img.loaded {
-		opacity: 1;
-	}
-
+    .pil-ph img.loaded {
+        opacity: 1;
+    }
 </style>
